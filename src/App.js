01ba8ddd6a06d16d47f8bb46e533083ee465a4b4
Yoav Karpassi <yoavkarp@gmail.com>
2024-05-16 18:43:14 +0300

    Firebase Initialize

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import LoginPage from './pages/login';
+import HomePage from './pages/home';
+import MyProfilePage from './pages/my-profile';
+import Header from './components/header'
 
 function App() {
   return (
-    <main>
-
-    </main>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="profile" element={<MyProfilePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
